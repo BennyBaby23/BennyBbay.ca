f1e9a5d8bcfd9394bd9e434646684c08f8a6eef5
Benny Baby <bennyb@Bennys-MacBook-Air.local>
2023-09-16 12:12:15 -0400

    added name and navbar

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import { motion } from "framer-motion";
+import { styles } from "../styles";
+import { ComputersCanvas } from "./canvas";
 
 const Hero = () => {
   return (
-    <div>Hero</div>
-  )
-}
+    <section className={`relative w-full h-screen mx-auto`}>
 
-export default Hero
+    <div className={`absolute inset-0 top-[120px]  max-w-7xl mx-auto ${styles.paddingX} flex flex-row items-start gap-5`}>
+
+      {/* dot and the purpple line */}
+      <div className='flex flex-col justify-center items-center mt-5'>
+        <div className='w-5 h-5 rounded-full bg-[#915EFF]' />
+        <div className='w-1 sm:h-80 h-40 violet-gradient' />
+      </div>
+
+      {/* Name and details */}
+      <div>
+      <a href="https://git.io/typing-svg"><img src="https://readme-typing-svg.demolab.com?font=Times+New+Roman&size=58&duration=5017&pause=1013&color=6BB5F7&width=582&height=82&lines=HI%2C+I'M+BENNY+BABY;ENTREPRENEUR;FULL+STACK+DEV;ALWAYS+LEARNING" alt="Typing SVG" /></a>
+        <p className={`${styles.heroSubText} mt-2 text-white-25`}>I started my coding journey as a naive computer science student with a passion to learn everything I could about this programming world - code, unix, linux, theory. And all the while, teaching myself iOS development with a dream to build my own app, but that soon got overshadowed by my desire to excel in Java. However, I think it's time. It's time to get uncomfortable again. I have a burning desire to be a full fledge Full stack developer and an Entrepreneur, and fulfill that dream younger me had of building my own company, my own product <br className="sm:block hidden"/>  And in order to do that, I'll be implmementing a few measures to focus more time on fulfilling that dream - a dream that I'll be ready to tackle in 2023 due to the measure I'm putting in place now until the end of 2022. Don't wait up, because I'm coming.</p>
+      </div>
+    </div>
+
+    <ComputersCanvas />
+    </section>
+  );
+};
+
+export default Hero;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,52 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+
+import { styles } from '../styles';
+import { navLinks } from '../constants';
+import { logo, menu, close} from '../assets';
+
+
 
 const Navbar = () => {
+
+  // use state field
+  const [active, setActive] = useState('');
+  const [toggle, setToggle] = useState(false);
+
   return (
-    <div>Navbar</div>
+    <nav className= {` ${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary`}>
+      <div className="w-full flex justify-between items-center max-w-7xl mx-auto">
+
+        {/* help us to nav where we are currently on page */}
+        <Link to="/" className="flex items-center gap-2" onClick={ () => {setActive(""); window.scrollTo(0, 0);  }}>
+        <img src={logo} alt="logo" className="w-12 h-12 object-contain"/>
+        <p className='text-white text-[18px] font-bold cursor-pointer flex'>Benny Baby</p>
+        </Link>
+
+        {/* Nav for home about and other details*/}
+        <ul className="list-none hidden sm:flex flex-row gap-10">
+          {navLinks.map((link) => (<li key={link.id} className={`${active === link.title? "text.white" : "text-secondary"} hover:text-white text-[17px] font-medium cursor-pointer`} onClick={() => setActive(link.title)}> <a href={`#${link.id}`}> {link.title} </a></li>))}
+        </ul>
+
+         {/* Nav for small devices to show in a block open and close*/}
+        <div className ="sm:hidden flex flex-1 justify-end items-center"> 
+        <img src={toggle ? close : menu} alt="menu" className="w-[28px] h-28px] object-contin cursor-pointer" onClick={() =>setToggle(!toggle)}/>
+        <div className={`${!toggle? 'hidden' : 'felx'} p-6 black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] z-10 rounded-xl`}>
+ 
+        {/* same Nav for home about and other details up but this for small device after clicking the toggle menu*/}
+        <ul className="list-none flex justify-end items-start flex-col gap-4">
+        {navLinks.map((link) => (<li key={link.id} className={`${active === link.title? "text.white" : "text-secondary"} font-poppins font-medium cursor-pointer text-[16px]`} onClick={() => {setToggle(!toggle); setActive(link.title);} }> <a href={`#${link.id}`}> {link.title} </a></li>))}
+        </ul>
+
+        </div>
+        </div>
+
+
+     
+
+
+      </div>
+    </nav>
   )
 }
 
