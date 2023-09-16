b36db767d2b7d778ed549682a016b0664373eed2
Benny Baby <bennyb@Bennys-MacBook-Air.local>
2023-09-16 14:33:36 -0400

    added 3d objects

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -17,7 +17,7 @@ const Hero = () => {
       {/* Name and details */}
       <div>
       <a href="https://git.io/typing-svg"><img src="https://readme-typing-svg.demolab.com?font=Times+New+Roman&size=58&duration=5017&pause=1013&color=6BB5F7&width=582&height=82&lines=HI%2C+I'M+BENNY+BABY;ENTREPRENEUR;FULL+STACK+DEV;ALWAYS+LEARNING" alt="Typing SVG" /></a>
-        <p className={`${styles.heroSubText} mt-2 text-white-25`}>I started my coding journey as a naive computer science student with a passion to learn everything I could about this programming world - code, unix, linux, theory. And all the while, teaching myself iOS development with a dream to build my own app, but that soon got overshadowed by my desire to excel in Java. However, I think it's time. It's time to get uncomfortable again. I have a burning desire to be a full fledge Full stack developer and an Entrepreneur, and fulfill that dream younger me had of building my own company, my own product <br className="sm:block hidden"/>  And in order to do that, I'll be implmementing a few measures to focus more time on fulfilling that dream - a dream that I'll be ready to tackle in 2023 due to the measure I'm putting in place now until the end of 2022. Don't wait up, because I'm coming.</p>
+        <p className={`${styles.heroSubText} mt-2 text-white-25`}>Printf (Hello World)</p>
       </div>
     </div>
 
--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -1,9 +1,39 @@
-import React from 'react'
+import React, { Suspense, useEffect, useState } from "react";
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
 
-const Computers = () => {
+import CanvasLoader from "../Loader";
+
+const Computers = ({ isMobile }) => {
+  const computer = useGLTF("./desktop_pc/scene.gltf");
+  console.log(computer);
+  
+    return (
+      <mesh>
+        <hemisphereLight intensity={2.15} groundColor="red" />
+        <pointLight intensity={1} />
+        <spotLight position={[-20, 50, 10]} angle={0.12} penumbra={1} intensity={5} castShadow shadow-mapSize={1024} />
+        <primitive object={computer.scene} scale={7.9} position={[-2, -3.25, -1.15]} rotation={[-0.01, -5.0, 0]} />
+      </mesh>
+    );
+};
+
+const ComputersCanvas = () => {
+
   return (
-    <div>Computers</div>
-  )
-}
+    <Canvas frameloop="demand" shadows camera={{ position: [20, 3, 5], fov:27}} gl={{preserveDrawingBuffer: true }}>
+      <Suspense fallback={<CanvasLoader />}>
+        <OrbitControls
+          enableZoom={false}
+          maxPolarAngle={Math.PI / 2}
+          minPolarAngle={Math.PI / 2}
+        />
+        <Computers />
+      </Suspense>
 
-export default Computers
+      <Preload all />
+    </Canvas>
+  );
+};
+
+export default ComputersCanvas;
